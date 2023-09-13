24661e45bb69f84ece6e2524fc9b144363fd4952
Juan Manuel De La Torre Sanchez <jdela@unal.edu.co>
2023-09-12 22:17:18 -0500

    github actions yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://turing-core-team.github.io/web-page-new/',
+    base: '/web-page-new'
+});
